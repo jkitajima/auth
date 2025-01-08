93c9b282849160e38416ad712b98aad16ae7862d
J. Kitajima <jvt_kitajima@icloud.com>
2025-01-07 21:43:24 -0300

    refactor: Changes on form decode

--- internal/auth/httphandler/request_access_token.go
+++ internal/auth/httphandler/request_access_token.go
@@ -19,11 +19,11 @@ func (s *AuthServer) handleRequestAccessToken() http.HandlerFunc {
 	const self = "handleRequestAccessToken"
 	const tracename string = "auth_request_acess_token"
 
-	// type request struct {
-	// 	GrantType string
-	// 	Username  string
-	// 	Password  string
-	// }
+	type request struct {
+		GrantType string
+		Username  string
+		Password  string
+	}
 
 	type response struct {
 		AccessToken  string `json:"access_token"`
@@ -32,47 +32,51 @@ func (s *AuthServer) handleRequestAccessToken() http.HandlerFunc {
 		ExpiresIn    int    `json:"expires_in"`
 	}
 
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx, span := s.tracer.Start(r.Context(), tracename)
-		defer span.End()
-
+	decodeForm := func(r *http.Request) (request, error) {
 		// Content-Type must be "application/x-www-form-urlencoded"
 		if ctype := r.Header.Get("Content-Type"); ctype != "application/x-www-form-urlencoded" {
-			span.SetStatus(codes.Error, fmt.Sprintf("%s failed", tracename))
-			span.RecordError(fmt.Errorf("Content-Type was %s", ctype))
-			responder.RespondMetaMessage(w, r, http.StatusBadRequest, "Content-Type must be application/x-www-form-urlencoded")
-			return
+			return request{}, fmt.Errorf("Content-Type must be application/x-www-form-urlencoded")
 		}
 
 		// Now that we know that the Content-Type is correct,
 		// we validate the form values
 		grantType := r.FormValue("grant_type")
 		if grantType != "password" {
-			span.SetStatus(codes.Error, fmt.Sprintf("%s failed", tracename))
-			span.RecordError(fmt.Errorf("Content-Type was %s", grantType))
-			responder.RespondMetaMessage(w, r, http.StatusBadRequest, "grant_type must be password")
-			return
+			return request{}, fmt.Errorf("grant_type must be password")
 		}
 
 		username := r.FormValue("username")
 		if username == "" {
-			span.SetStatus(codes.Error, fmt.Sprintf("%s failed", tracename))
-			span.RecordError(fmt.Errorf("username was empty"))
-			responder.RespondMetaMessage(w, r, http.StatusBadRequest, "username must not be empty")
-			return
+			return request{}, fmt.Errorf("username must not be empty")
 		}
 
 		password := r.FormValue("password")
 		if password == "" {
+			return request{}, fmt.Errorf("password must not be empty")
+		}
+
+		return request{
+			GrantType: grantType,
+			Username:  username,
+			Password:  password,
+		}, nil
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx, span := s.tracer.Start(r.Context(), tracename)
+		defer span.End()
+
+		req, err := decodeForm(r)
+		if err != nil {
 			span.SetStatus(codes.Error, fmt.Sprintf("%s failed", tracename))
-			span.RecordError(fmt.Errorf("password was empty"))
-			responder.RespondMetaMessage(w, r, http.StatusBadRequest, "password must not be empty")
+			span.RecordError(err)
+			responder.RespondMetaMessage(w, r, http.StatusBadRequest, err.Error())
 			return
 		}
 
 		requestAcessTokenResponse, err := s.service.RequestAccessToken(ctx, auth.AccessTokenRequest{
-			Username: username,
-			Password: password,
+			Username: req.Username,
+			Password: req.Password,
 		})
 		if err != nil {
 			span.SetStatus(codes.Error, fmt.Sprintf("%s failed", tracename))
